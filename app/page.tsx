246a6aa9a6c1356bed686dbceef95c285730f5dc
Isaac Perez <terepaimawork@gmail.com>
2026-01-29 11:26:16 +0100

    Tailwind configured

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,7 @@ export default function Home() {
           priority
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <h1 className="text-8xl font-bold underline">Hello, Next.js!</h1>
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             To get started, edit the page.tsx file.
           </h1>
