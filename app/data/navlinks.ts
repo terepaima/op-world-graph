ae2d963a329183407904474c0db13e8c7a74f929
Isaac Perez <terepaimawork@gmail.com>
2026-01-30 11:27:08 +0100

    #3 adding navbar

---
+++ app/data/navlinks.ts
@@ -0,0 +1,6 @@
+import { INavLink } from "@/types/types";
+
+export const navlinks: INavLink[] = [
+    { name: "Home", href: "/" },
+    { name: "World", href: "#graph" },
+];
--- app/graph/page.tsx
+++ app/graph/page.tsx
@@ -5,7 +5,9 @@ export default function Home() {
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="text-8xl font-bold underline">Hello, Next.js!</h1>
+          <h2 className="text-2xl font-semibold"> Graph Stage</h2>
+          <h2 className="text-2xl font-semibold">Details Panel</h2>
+          <h2 className="text-2xl font-semibold">Control Panel</h2>
         </div>
       </main>
     </div>
