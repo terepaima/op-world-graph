7d5dc698c4d247f939d7f22f9558db8f1c18cbdd
Isaac Perez <terepaimawork@gmail.com>
2026-02-12 15:09:24 +0100

    #18 Adjust navbar styles and mobile view

--- app/components/layout/Navbar.tsx
+++ app/components/layout/Navbar.tsx
@@ -25,16 +25,16 @@ export default function Navbar() {
         viewport={{ once: true }}
         transition={{ type: 'spring', stiffness: 250, damping: 70, mass: 1 }}
       >
-        <a href="https://prebuiltui.com?utm_source=pixels">
+        <Link onClick={() => onNavigate('/#')} href="/#" className="transition">
           <Image
             className="h-8.5 w-auto"
-            src="/assets/logo.svg"
+            src="/logo.png"
             alt="logo"
-            width={130}
-            height={34}
+            width={40}
+            height={24}
             priority
           />
-        </a>
+        </Link>
 
         <div className="hidden md:flex items-center gap-8 transition duration-500">
           {navlinks.map((link: INavLink) => (
@@ -42,23 +42,19 @@ export default function Navbar() {
               onClick={() => onNavigate(link.href)}
               key={link.name}
               href={link.href}
-              className="hover:text-pink-500 transition"
+              className="hover:text-green-500 transition"
             >
               {link.name}
             </Link>
           ))}
         </div>
-
-        <button className="hidden md:block px-6 py-2.5 bg-pink-600 hover:bg-pink-700 active:scale-95 transition-all rounded-full">
-          Start free trial
-        </button>
         <button onClick={() => setIsOpen(true)} className="md:hidden">
           <MenuIcon size={26} className="active:scale-90 transition" />
         </button>
       </motion.nav>
 
       <div
-        className={`fixed inset-0 z-100 bg-black/40 backdrop-blur flex flex-col items-center justify-center text-lg gap-8 md:hidden transition-transform duration-400 ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}
+        className={`fixed inset-0 z-[45] bg-black/40 backdrop-blur flex flex-col items-center justify-center text-lg gap-8 md:hidden transition-transform duration-400 ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}
       >
         {navlinks.map((link: INavLink) => (
           <Link
@@ -72,7 +68,7 @@ export default function Navbar() {
         ))}
         <button
           onClick={() => setIsOpen(false)}
-          className="active:ring-3 active:ring-white aspect-square size-10 p-1 items-center justify-center bg-pink-600 hover:bg-pink-700 transition text-white rounded-md flex"
+          className="active:ring-3 active:ring-white aspect-square size-10 p-1 items-center justify-center bg-green-600 hover:bg-green-700 transition text-white rounded-md flex"
         >
           <XIcon />
         </button>
